715f33de253e3b40a91014e8e0709b3d4ff4b2eb
LucasAp-DEV <lucasapdev@gmail.com>
2023-07-25 13:20:33 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Login from './pages/Login'
+import User from './pages/Login';
 
 function App() {
   return (
       <div>
-         <Login/>
+         <User/>
       </div>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import './Login.css'
+import React from 'react'
+
+const User = () => {
+
+    const form = () => {
+        return(
+            <h1>
+                Hello world
+            </h1>
+        )
+    }
+    
+    return (
+            <div>
+                {form()}
+            </div>
+        );
+    }
+
+
+export default User;

e5b07927acefdc192f2ee1354253047d5b17f957
LucasAp-DEV <lucasapdev@gmail.com>
2023-07-26 13:30:41 -0300

    add

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ const User = () => {
     const cabecario = () => {
         return(
             <div className='destaque'>
-                <img src="sesmt.jpg" alt="Minha imagen" width="200" height="175"/>
+                <img src="sesmit1.jpg" alt="Minha imagen" width="200" height="175"/>
             </div>
         )
     }

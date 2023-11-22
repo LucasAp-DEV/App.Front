0eba3daaca9e2c9bc9650a817405da78f1d92477
LucasAp-DEV <lucasapdev@gmail.com>
2023-11-22 15:48:45 -0300

    comit teste

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 import User from './pages/Login';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+
   return (
+
       <div>
          <User/>
+
+         <button onClick={() => {
+              setCount(prev => prev + 1)
+         }}> horas
+        </button>
+
+        {count}
+
+        <button onClick={() => {
+              setCount(prev => prev - 1)
+         }}> banco
+        </button>
+
+        
+        
       </div>
   );
 }
